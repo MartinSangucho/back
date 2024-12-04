d09c1d9abc4c025551ce520e3539f590e4e67751
MartinSangucho <lmu.sangucho@yavirac.edu.ec>
2024-12-03 19:58:05 -0600

    clase 15 back

---
+++ src/modules/categoria/categoria.service.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateCategoriaDto } from './dto/create-categoria.dto';
+import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { Repository } from 'typeorm';
+import { Categoria } from './entities/categoria.entity';
+
+@Injectable()
+export class CategoriaService {
+
+  constructor(@Inject('CATEGORIA_REPOSITORY') private categoriaRepository:Repository<Categoria>){}
+    
+  async create(createCategoriaDto: CreateCategoriaDto) {
+    const categoria = new Categoria()
+    categoria.nombre = createCategoriaDto.nombre
+    categoria.detalle = createCategoriaDto.detalle
+    return await this.categoriaRepository.save(categoria)
+}
+
+  async findAll() {
+    return await this.categoriaRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} categoria`;
+  }
+
+  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
+    return `This action updates a #${id} categoria`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} categoria`;
+  }
+}
